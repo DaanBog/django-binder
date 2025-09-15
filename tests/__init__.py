f94d9f288e40540ac34c416b89c8b59ac4522ae8
knokko <tim@codeyellow.nl>
2025-09-15 10:23:28 +0200

    T50470: push websocket updates upon saving models

--- tests/__init__.py
+++ tests/__init__.py
@@ -100,7 +100,11 @@ if not settings.configured:
 			# Basic permissions which can be used to override stuff
 			'testapp.view_country': [
 
-			]
+			],
+			'testapp.manage_country': [
+				('testapp.view_country', 'all'),
+				('testapp.view_city', 'all'),
+			],
 		},
 		'GROUP_PERMISSIONS': {
 			'admin': [
